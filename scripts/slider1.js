f5ba00285a49e7ccd927a9c9d9ecaef362aac722
SergeyKulikov01 <77584184+SergeyKulikov01@users.noreply.github.com>
2021-08-20 23:49:56 +0300

    Add files via upload

---
+++ scripts/slider1.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($){
+	"use strict";
+      $('.slider').bxSlider({
+		  preloadImages: 'all',
+		  auto: true,
+		  pause: 6000,
+		  slideWidth: 1920,
+	  });
+    });
